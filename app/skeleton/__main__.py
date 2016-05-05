faf7f51274a59afa42c4c31354d630e9c471f764
Václav Klusák <vaclav.klusak@klokantech.com>
2016-05-05 10:44:43 +0200

    Serve static files through the reverse proxy in production.

--- app/skeleton/__main__.py
+++ app/skeleton/__main__.py
@@ -1,3 +1,5 @@
+from shutil import copytree
+
 from flask.ext.migrate import MigrateCommand
 from flask.ext.script import Manager, Server, Shell
 
@@ -17,6 +19,12 @@ def shell_context():
         'db': db,
         'model': model,
     }
+
+
+@manager.command
+def export_static(target):
+    """Export the static directory."""
+    copytree(app.static_folder, target)
 
 
 def main():
