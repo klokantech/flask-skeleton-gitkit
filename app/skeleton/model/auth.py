87d269079dcd1de1f46eaa2e1b5a6aa6b7839bde
Oldrich Vykydal <info@oldrichvykydal.cz>
2016-08-19 15:26:12 +0200

    Datatype change

--- app/skeleton/model/auth.py
+++ app/skeleton/model/auth.py
@@ -10,7 +10,7 @@ class Account(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     gitkit_id = db.Column(db.Text, unique=True)
     email = db.Column(db.Text, unique=True, nullable=False)
-    email_verified = db.Column(db.Text, default=False, nullable=False)
+    email_verified = db.Column(db.Boolean, default=False, nullable=False)
     email_challenged = db.Column(db.DateTime())
     name = db.Column(db.Text)
     photo_url = db.Column(db.Text)
